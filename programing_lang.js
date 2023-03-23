dd93c571bf21c76400fa416195cded9d1c45e25b
AlePaff <alejandropaff@gmail.com>
2023-03-23 00:45:10 -0300

    imagenes cambiadas de carpeta

--- programing_lang.js
+++ programing_lang.js
@@ -1,29 +1,30 @@
 
 //dado un array ej. ["html", "css", "js"] devuelve una etiqueta html con los iconos de los lenguajes
 function lenguajes(array_lang) {
+    const ICON_PATH = "assets/icons/";
 
     const icons = {
-        html: "icons/html5.png",
-        css: "icons/css.png",
-        js: "icons/js.png",
-        // js: "./icons/js_alt.png",
-        boostrap: "icons/boostrap.png",
-        // boostrap: "icons/boostrap_alt.png",
-        isotope: "icons/isotope.ico",
-        python: "icons/python.png",
-        jquery: "icons/jquery.png",
-        c: "icons/c.png",
-        gitflow: "icons/gitflow.png",
-        java: "icons/java.png",
-        chakra: "icons/chakra.png",
-        react: "icons/react.ico",
-        // react: "icons/react.svg",
-        rust: "icons/rust.png",
-        vite: "icons/vite.svg",
-        vitest: "icons/vitest.ico",
-        tailwind: "icons/tailwind.ico",
-        typescript: "icons/typescript.png",
-        scss: "icons/scss.png",
+        html: ICON_PATH + "html5.png",
+        css: ICON_PATH + "css.png",
+        js: ICON_PATH + "js.png",
+        // js: ".ICON_PATH + /js_alt.png",
+        boostrap: ICON_PATH + "boostrap.png",
+        // boostrap: ICON_PATH + "boostrap_alt.png",
+        isotope: ICON_PATH + "isotope.ico",
+        python: ICON_PATH + "python.png",
+        jquery: ICON_PATH + "jquery.png",
+        c: ICON_PATH + "c.png",
+        gitflow: ICON_PATH + "gitflow.png",
+        java: ICON_PATH + "java.png",
+        chakra: ICON_PATH + "chakra.png",
+        react: ICON_PATH + "react.ico",
+        // react: ICON_PATH + "react.svg",
+        rust: ICON_PATH + "rust.png",
+        vite: ICON_PATH + "vite.svg",
+        vitest: ICON_PATH + "vitest.ico",
+        tailwind: ICON_PATH + "tailwind.ico",
+        typescript: ICON_PATH + "typescript.png",
+        scss: ICON_PATH + "scss.png",
     };
     
     let total = "<div id='imagenes'>";
